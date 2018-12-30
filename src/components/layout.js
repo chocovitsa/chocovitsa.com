b8ecd2d7b558bda78f55f75faab3b7f88149c90d
chocovitsa <hrabur@gmail.com>
2018-12-30 16:28:29 +0200

    Add SEO metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import { IntlProvider, addLocaleData } from 'react-intl'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../scss/agency.scss'
+import favicon from '../images/favicon.png'
 
 class Layout extends React.Component {
   render() {
@@ -16,11 +17,33 @@ class Layout extends React.Component {
       <IntlProvider locale={locale} messages={messages}>
         <div>
           <Helmet
+            htmlAttributes={{
+              lang: locale,
+              dir: 'ltr',
+            }}
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Chovitsa' },
-              { name: 'keywords', content: 'sample, something' },
+              {
+                name: 'title',
+                content:
+                  'Colourfull and modern chocolates, reshaping Bulgarian traditional embroidery in completely new way!',
+              },
+              {
+                name: 'description',
+                content:
+                  'Colourfull and modern chocolates, reshaping Bulgarian traditional embroidery in completely new way!',
+              },
+              {
+                name: 'keywords',
+                content:
+                  'chocovitsa, chocovitsa.com, chocolate, bulgarian traditional embroidery, hand made, rich taste, stylish',
+              },
+              {
+                name: 'google-site-verification',
+                content: 'MQN82vrvPE1iL7uHXMd0IC6vklEjUdrL3RG5HtCg21g',
+              },
             ]}
+            link={[{ rel: 'shortcut icon', href: favicon }]}
           />
           {children}
         </div>
